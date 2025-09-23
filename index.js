44f027d538eb1cc8f55d5a0ff3b551191bf3445b
Edward Hollmon <edhollmon@gmail.com>
2025-09-23 04:21:31 -0700

    log cache results

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
 import { Client } from '@elastic/elasticsearch';
+import { createClient } from 'redis';
 import 'dotenv/config';
 
 const app = express();
 const port = 3001
+const cacheKeyPrefix = 'events:tenant:'
+
+// Setup Redis
+const redisClient = createClient();
+redisClient.on('error', err => console.log('Redis Client Error', err));
+await redisClient.connect();
 
 // Elasticsearch setup
 const node = 'http://localhost:9200';
-const client = new Client({
+const esClient = new Client({
   node,
   auth: {
     apiKey: process.env.ELASTICSEARCH_API_KEY,
   }
 })
 const index = 'audit-events'
-// await client.indices.create({ index })
+// await esClient.indices.create({ index })
 
 
 // middleware to parse JSON request body
@@ -28,10 +35,26 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/audit-event', async (req, res) => {
-    const result = await client.search({
+    const tenantId = 123
+    const cacheKey = `${cacheKeyPrefix}${tenantId}`
+    
+    const cachedResults = await redisClient.lRange(cacheKey, 0, -1);
+
+    // Parse each JSON string back into an object
+    const cachedEvents = cachedResults.map(JSON.parse);
+    console.log("Cached Events: (NOT RETURNING)")
+    console.log(cachedEvents);
+
+    /*
+    if(cachedEvents){
+      return res.json(cachedEvents)
+    }
+    */
+
+    const result = await esClient.search({
         query: {
             match: {
-                "tenantId": 123
+                "tenantId": tenantId
             }
         }
     })
